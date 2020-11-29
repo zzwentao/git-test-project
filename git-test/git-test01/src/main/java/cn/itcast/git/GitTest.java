41b268e5ce66862099af1a7f1ad09cf2e7282de4
myGit <myGit@>
2020-11-29 19:38:06 +0800

    IDEA第一次提交!

---
+++ git-test/git-test01/src/main/java/cn/itcast/git/GitTest.java
@@ -0,0 +1,18 @@
+package cn.itcast.git;
+
+/**
+ * @description:
+ * @author: Mr.zhou
+ * @createDate: 2020/11/29
+ * @version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+            System.out.println("git............................");
+            System.out.println("git............................");
+            System.out.println("git............................");
+            System.out.println("git............................");
+            System.out.println("git............................");
+            System.out.println("git............................");
+        }
+}
